bc3bed93e29d5f19174a412e8e29a0a9a960eede
amal-kg <amalkg80333@gmail.com>
2023-11-04 16:20:28 -0400

    Logout  btn added.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,69 +1,101 @@
-import React from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
-import './Navbar.css'; // Import the CSS file
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-function Navbar() {
-  const handleClick = () => {
-    // Your click handler code here
-  };
+import "./Navbar.css";
 
+
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const[email,setEmail]=useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
+       
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+          }
+        }
+        setEmail('');
+        window.location.reload();
+    }
+    const handleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    }
+    useEffect(() => { 
+      const storedemail = sessionStorage.getItem("email");
+
+      if (storedemail) {
+            setIsLoggedIn(true);
+            setUsername(storedemail);
+          }
+        }, []);
   return (
     <nav>
       <div className="nav__logo">
-        <a href="/">
-          StayHealthy{' '}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="26"
-            width="26"
-            viewBox="0 0 1000 1000"
-            style={{ fill: '#3685fb' }}
-          >
-            <title>Doctor With Stethoscope SVG icon</title>
-            <g>
-              <g>
-                <path d="M499.8,10c91.7,0,166,74.3,166,166c0,91.7-74.3,166-166,166c-91.7,0-166-74.3-166-166C333.8,84.3,408.1,10,499.8,10z"></path>
-                <path d="M499.8,522.8c71.2,0,129.1-58.7,129.1-129.1H370.6C370.6,464.1,428.6,522.8,499.8,522.8z"></path>
-                <path d="M693.2,395c-0.7,94.9-70.3,173.7-160.8,188.9v155.9c0,80.3-60.7,150.8-140.8,155.3c-83,4.7-152.7-58.9-157.6-139.7c-22-12.8-35.6-38.5-30.3-66.7c-4.7-25.1,25.5-45.6,50.8-49.9c-23.7,34.6-68.3,58.9-116.7,58.9c-90.5,0-164.4-73.9-164.4-164.4v-158c-90.5-15.2-160.1-94-160.8-188.9c-89.4,11.5-158.2,87.9-158.2,180.5V910c0,44.2,35.8,80,80,80h542.6c44.2,0,80.1-35.8,80.1-80V575.5C851.4,483,782.6,406.6,693.2,395z"></path>
-              </g>
-            </g>
-          </svg>
-        </a>
+        <Link to="/">
+        StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i></Link>
         <span>.</span>
       </div>
       <div className="nav__icon" onClick={handleClick}>
-        <i className="fa fa-times fa fa-bars"></i>
+        <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
       </div>
-      <ul className="nav__links active">
+      <ul className={click ? 'nav__links active' : 'nav__links'}>
         <li className="link">
-          <a href="../Landing_Page/LandingPage.html">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li className="link">
-          <a href="#">Appointments</a>
-        </li>
-        {/* <li className="link">
-          <a href="../Sign_Up//Sign_Up.html">
-            <button className="btn1">Sign Up</button>
-          </a>
+          <Link to="/search/doctors">Appointments</Link>
         </li>
         <li className="link">
-          <a href="../Login/Login.html">
-            <button className="btn1">Login</button>
-          </a>
-        </li> */}
+          <Link to="/healthblog">Health Blog</Link>
+        </li>
         <li className="link">
-            <Link to="/signup">
-              <button className="btn1">Sign Up</button>
-            </Link>
-          </li>
-          <li className="link">
-            <Link to="/login">
-              <button className="btn1">Login</button>
-            </Link>
-          </li>
+         <Link to="/reviews">Reviews</Link>
+        </li>
+        {isLoggedIn?(
+          <>
+            <li className="link">
+              <button className="btn2" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+            
+          </>
+        ) : (
+          <>
+            <li className="link">
+              <Link to="/signup">
+                <button className="btn1">Sign Up</button>
+              </Link>
+            </li>
+            <li className="link">
+              <Link to="/login">
+                <button className="btn1">Login</button>
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
+
--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,132 +1,132 @@
 import React, { useState } from 'react';
-import './Sign_Up.css'; // Import the CSS file 
+import './Sign_Up.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
-function SignUp() {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phoneError, setPhoneError] = useState('');
+const Sign_Up = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
 
-  const handlePhoneChange = (event) => {
-    const phoneNumber = event.target.value;
+    const navigate = useNavigate();
 
-    // Remove non-digit characters from the input
-    const cleanedPhoneNumber = phoneNumber.replace(/\D/g, '');
+    const register = async (e) => {
+        e.preventDefault();
 
-    // Check if the cleaned phone number has exactly 10 digits
-    if (cleanedPhoneNumber.length === 10) {
-      setPhone(cleanedPhoneNumber);
-      setPhoneError('');
-    } else {
-      setPhone(cleanedPhoneNumber);
-      setPhoneError('Please enter a valid 10-digit phone number.');
-    }
-  };
+        // API Call
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+            }),
+        });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+        const json = await response.json();
 
-    if (phone.length !== 10) {
-      setPhoneError('Please enter a valid 10-digit phone number.');
-      return;
-    }
+        if (json.authtoken) {
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+            navigate("/"); // Redirect to home page
+            window.location.reload();
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg);
+                }
+            } else {
+                setShowerr(json.error);
+            }
+        }
+    };
 
-    // Continue with form submission if phone number is valid
-  };
-
-  return (
-    <div className="container" style={{ marginTop: '5%' }}>
-      <div className="signup-grid">
-        <div className="signup-text">
-          <h1>Sign Up</h1>
+    return (
+        <div className="container" style={{ marginTop: '10%' }}>
+            <div className="signup-grid">
+                <div className="signup-form">
+                    <form method="POST" onSubmit={register}>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="form-control"
+                                placeholder="Enter your name"
+                                aria-describedby="helpId"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone</label>
+                            <input
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                type="tel"
+                                name="phone"
+                                id="phone"
+                                className="form-control"
+                                placeholder="Enter your phone number"
+                                aria-describedby="helpId"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                aria-describedby="helpId"
+                            />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                name="password"
+                                id="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                aria-describedby="helpId"
+                            />
+                        </div>
+                        {/* Add the rest of the form fields here */}
+                        <div className="btn-group">
+                            <button
+                                type="submit"
+                                className="btn btn-primary mb-2 mr-1 waves-effect waves-light"
+                            >
+                                Submit
+                            </button>
+                            <button
+                                type="reset"
+                                className="btn btn-danger mb-2 waves-effect waves-light"
+                            >
+                                Reset
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="signup-text1" style={{ textAlign: 'left' }}>
-          Already a member?{' '}
-          <span>
-            <a href="../Login/Login.html" style={{ color: '#2190FF' }}>
-              Login
-            </a>
-          </span>
-        </div>
-        <div className="signup-form">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                required
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-control"
-                placeholder="Enter your name"
-                aria-describedby="helpId"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phone">Phone</label>
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                required
-                value={phone}
-                onChange={handlePhoneChange}
-                className="form-control"
-                placeholder="Enter your phone number"
-                aria-describedby="helpId"
-              />
-              {phoneError && <span className="error-message">{phoneError}</span>}
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control"
-                placeholder="Enter your email"
-                aria-describedby="helpId"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-control"
-                placeholder="Enter your password"
-                aria-describedby="helpId"
-              />
-            </div>
-            <div className="btn-group">
-              <button
-                type="submit"
-                className="btn btn-primary mb-2 mr-1 waves-effect waves-light"
-              >
-                Submit
-              </button>
-              <button
-                type="reset"
-                className="btn btn-danger mb-2 waves-effect waves-light"
-              >
-                Reset
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-export default SignUp;
+export default Sign_Up;
